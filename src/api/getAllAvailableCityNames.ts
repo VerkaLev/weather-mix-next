37c2fe1503ed1a681be97ceb0d71e3f207713816
Veronika Klevko <90059559+VerkaLev@users.noreply.github.com>
2025-09-15 22:30:35 +0200

    https replaced with http as required by api

--- src/api/getAllAvailableCityNames.ts
+++ src/api/getAllAvailableCityNames.ts
@@ -5,7 +5,7 @@ export async function getAllAvailableCityNames(
   locale: string
 ) {
   const dataAvailableCityNames = await getData(
-    `https://api.geonames.org/searchJSON?name_startsWith=${letters}&maxRows=10&lang=${locale}&username=verkalev&featureClass=P`,
+    `http://api.geonames.org/searchJSON?name_startsWith=${letters}&maxRows=10&lang=${locale}&username=verkalev&featureClass=P`,
     { next: { revalidate: 21600 } }
   );
 
